763b31f27929248e2761b7165a0ce71b4def76a5
vaibhav-rm <rathodvaibhav401@gmail.com>
2025-11-21 23:03:45 +0530

    21 Nov 2025

---
+++ arrays/BruteTwoSum.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> findTwoSum(vector<int> arr, int target) {
+  vector<int> result;
+  for (int i = 0; i < arr.size(); i++) {
+    for (int j = i+1; j < arr.size(); j++) {
+      if (target == (arr[i] + arr[j])) {
+        result.push_back(i);
+        result.push_back(j);
+      }
+    }
+  }
+  if (result.size() != 0) {
+   return result;
+  }
+  result.push_back(-1);
+  return result;
+}
+
+int main() {
+  vector<int> arr = {2,6,5,8,11};
+  vector<int> res = findTwoSum(arr, 14);
+  for (auto it = res.begin(); it != res.end(); ++it) {
+    cout << *it << " ";
+  }
+}
